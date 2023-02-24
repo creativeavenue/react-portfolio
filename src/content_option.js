6bc80468fb22a1c6be11b8d0d12ab7a4c354ad0f
creativeavenue <51488537+creativeavenue@users.noreply.github.com>
2023-02-24 16:20:11 -0500

    Maintenance Page

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,18 @@
-const logotext = "JOHN";
+const logotext = "OMNIACENTRAL";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Omnia Central",
+    description: "Omnia Central - Simplify your IT Solutions.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "OMNIA CENTRAL",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "Simplified.",
+        second: "Secure.",
+        third: "Ready for anything.",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "Specialized solutions designed to help your organization protect critical assets from cyber attacks.",
+    your_img_url: "https://cyberforces.com/wp-content/themes/html5blank-stable/img/svgs/icon-main.svg",
 };
 
 const dataabout = {
